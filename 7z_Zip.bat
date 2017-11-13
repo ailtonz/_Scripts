d5c44a68c5d621e02080a0e1a38bc0cd3961e7d5
ailtonz <ailton@f4consult.com.br>
2017-11-13 12:03:16 -0200

    Comentarios e links adicionais

--- 7z_Zip.bat
+++ 7z_Zip.bat
@@ -1,1 +1,12 @@
-for /d %%X in (*) do "C:\Program Files (x86)\7-Zip\7z.exe" a "%%X.7z" "%%X\"
+rem ***
+rem *** Compactacao de arquivos em pasta ***
+rem ***
+
+rem https://winscp.net/eng/docs/scripting#commands
+rem https://www.dotnetperls.com/7-zip-examples
+rem https://ss64.com/nt/
+rem https://ss64.com/nt/for.html
+
+for /d %%X in (*) do "C:\tmp\7-Zip\7z.exe" a "%%X.7z" "%%X\"
+
+pause
